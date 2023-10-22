7294ab1d43aa7ed2882695003d218c54dd9e67d8
unknown <k.sunilkiran98@gmail.com>
2023-10-22 09:46:41 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'templatereference';
+
+  returnmessage:string='';
+  enteredelement:string='';
+
+  disp:string='';
+
+  welcomemessage:string="Hello Sunil";
+
+  Welcome(inputElement:HTMLInputElement){
+
+    // alert('Hi Sunil'+inputElement.value);
+    console.log(inputElement);
+    console.log(inputElement.value);
+    this.disp=inputElement.value;
+   // inputElement.serachelement;
+
+  }
+
+  // display(event:Event){
+
+  //   this.returnmessage= (event.target as HTMLInputElement).value;
+
+  // }
 }
